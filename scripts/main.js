6cc37bef96a43979a0d277d91f43e73a2eec19fe
Jevgeni Krais <jevgeni@pitfire.eu>
2014-11-01 22:14:18 +0200

    initial

---
+++ scripts/main.js
@@ -0,0 +1,51 @@
+(function($) {
+	
+	$(function() {
+
+		var url = document.URL;
+
+		$('ul#menu li a').click(function(e) {
+			if($(window).width() <= 800)
+			{
+				e.preventDefault();
+
+				url = document.URL;
+				$('#home-articles').addClass('active');
+				$('#articles-title #title').text($(this).text());
+				$('ul#articles').empty();
+
+				var catPosts = posts[$(this).text()];
+
+				for(var post in catPosts)
+				{
+					$('ul#articles').append('<li class="article">' +
+						'<div class="content">' +
+							'<a href="' + catPosts[post] + '">' + post + '</a>' +
+						'</div>' +
+					'</li>');
+				}
+
+				history.pushState({
+					page: $(this).text()
+				}, $(this).text(), $(this).text() + '.html')
+			}
+
+		});
+
+		$('#back').click(function() {
+			$('#home-articles').removeClass('active');
+
+			history.pushState({
+				page: '',
+			}, url, url)
+		});
+
+		if($('#splash').is(':visible'))
+		{
+			$('#splash').css('background-image', 'url(/images/' + $('#splash').data('bgr') + ')');
+			$('#splash').show();
+		}
+
+	});
+
+})(jQuery);
